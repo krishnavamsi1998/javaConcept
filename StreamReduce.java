45a516c6e51613aa061b6a6c3c563fe9e7af1977
krishnavamsi1998 <106548869+krishnavamsi1998@users.noreply.github.com>
2022-06-27 19:50:15 +0530

    Create StreamReduce.java

---
+++ StreamReduce.java
@@ -0,0 +1,24 @@
+package javaconcepts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamReduce {
+
+	public static void main(String[] args) {
+
+		List<Customer> list = new ArrayList<>();
+
+		// new Customer(name,score)
+		list.add(new Customer("vamsi", 100));
+		list.add(new Customer("asif", 200));
+		list.add(new Customer("ameer", 500));
+
+		// calculating sum of scores
+		// the parameters need not be actual names
+		Integer s = list.stream().map(a -> a.score).reduce(0, (sum, score) -> sum + score);
+		System.out.println(s);
+
+	}
+
+}
