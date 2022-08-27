4cfc907670032713bb150baf2196aeeff6e66716
krishnavamsi1998 <106548869+krishnavamsi1998@users.noreply.github.com>
2022-08-27 19:00:51 +0530

    Create NumberClass.java

---
+++ NumberClass.java
@@ -0,0 +1,14 @@
+//The abstract class Number is the 
+//superclass of classes BigDecimal, 
+//BigInteger, Byte, Double, Float, Integer,
+//Long, and Short.
+public class NumberClass {
+
+	public static void main(String[] args) {
+		Number i=10;
+		System.out.println(i.getClass());
+		
+
+	}
+
+}
